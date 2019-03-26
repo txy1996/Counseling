0fb467cccb282a028a71095a6fba68f909a7eda5
akashi <1272735947@qq.com>
2019-03-26 12:05:43 +0800

    测试提交

---
+++ src/main/java/com/akashi/counseling/utils/text.java
@@ -0,0 +1,13 @@
+package com.akashi.counseling.utils;
+
+/**
+ * FileName: text
+ * Author:   tangxiaoyu
+ * Date:     2019/3/26 12:02
+ * Description: TODO(这里用一句话描述这个类的作用)
+ *
+ * @since 1.0.0
+ */
+public class text {
+
+}
